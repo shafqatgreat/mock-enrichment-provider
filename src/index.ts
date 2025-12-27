a2b19d1a951059d8fc590d5f69257ac9f9a72caf
Shafqat Altaf <shafqat@yahoo.com>
2025-12-27 09:24:47 +0500

    First Final Commit on 27 Dec 2025

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,24 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
+// src/index.ts
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+  async fetch(request: Request) {
+    if (request.method !== "POST") {
+      return new Response("Method Not Allowed", { status: 405 });
+    }
+    console.log("Inside Provider:");
+    // Consume body so fetch pipeline completes correctly
+    // await request.json().catch(() => ({}));
+    // Read incoming client data
+    const clientData = await request.json().catch(() => ({}));
+    console.log("Received data from gateway:", clientData);
+
+    return new Response(
+      JSON.stringify({
+        mobile: "+923001234567",
+        confidence: 0.88,
+        request_id: crypto.randomUUID(),
+        receivedClientData: clientData, // Echo back client data
+      }),
+      { headers: { "Content-Type": "application/json" }, status: 200 }
+    );
+  }
+};
